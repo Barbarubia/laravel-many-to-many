7cdba03d23b97cf433c7ed6651d6157a76dff937
Barbarubia <dnl.desimone@gmail.com>
2022-05-25 12:58:34 +0200

    Modificati titoli views

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.app')
 
+@section('pageTitle', 'Home ' . Auth::user()->name)
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }} | @yield('pageTitle')</title>
+    <title>{{ config('app.name', 'BoolPress') }} | @yield('pageTitle')</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
